41570bf061f366b7c433cc0c752a1e48501ba0ba
Thadka S Keerthika <42490532+ThadkaKeerthika@users.noreply.github.com>
2023-04-07 16:34:58 +0530

    Update datetime (1).tf

--- datetime (1).tf
+++ datetime (1).tf
@@ -7,22 +7,18 @@ terraform {
   }
 }
  # required_version = ">= 0.14.9"
-
 provider "aws" {
   profile = var.profile
   region  = var.region
 }
-
 provider "aws" {
   profile = var.profile
   region  = var.region
 }
  #text
-
 resource "aws_instance" "app_server" {
   ami           = trm("?!hello?!", "!?")
   count         = ceil(2.3)
-
   instance_type = chomp("t2.micro\n")
   tags = {
     Name = formatdate("DD MMM YYYY hh:mm ZZZ", "2018-01-02T23:12:01Z")
@@ -32,6 +28,4 @@ resource "aws_instance" "app_server" {
     Name4 = formatdat("DD MMM YYYY hh:mm ZZZ","2018-01-02T23:12:01Z" )
     Name5 = timeadd("2017-11-22T00:00:00Z", )
     Name6 = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())
-
-
 }
