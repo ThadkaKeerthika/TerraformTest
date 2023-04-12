1ab76b4e07aacaf3c8de234736fe1dae4278cf62
Thadka S Keerthika <42490532+ThadkaKeerthika@users.noreply.github.com>
2023-04-12 13:48:57 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "unrestricted" {
   acl    = "public-read-write
   versioning {
     enabled = true
-  }
+  
   logging {
     target_bucket = aws_s3_bucket.log_bucket.id
     target_prefix = "log"
